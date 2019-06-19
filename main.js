13d5134761bb91dfcee53b8741e412a4c5a2519b
Gustavo Baccan Gomes <gustavo.gomes@butantan.gov.br>
2019-06-19 14:06:00 -0300

    Classes, extends, constructors...

--- main.js
+++ main.js
@@ -1,11 +1,40 @@
-alert('Teste')
-
-class Teste {
-    metodo() {
-
+class List {
+    constructor(){
+        this.data = []
     }
 
-    outro() {
-
+    add(data){
+        this.data.push(data)
+        console.log(this.data)
     }
 }
+// extends tem a função de copiar/realizar as mesmas funcionalidades de outra classe
+class TodoList extends List{
+    // constructor(){
+    //     this.todos = []
+    // }
+
+    // addTodo(){
+    //     this.todos.push('Novo todo')
+    //     console.log(this.todos)
+    // }
+
+    // super é responsável por reutilizar o método constructor da classe extendida
+    constructor(){
+        super()
+
+        this.usuario = 'Baccan'
+    }
+
+    mostrausuario() {
+        console.log(this.usuario)
+    }
+}
+
+const MinhaLista = new TodoList()
+
+document.querySelector('#novoTodo').onclick = function() {
+    MinhaLista.add('Novo todo')
+}
+
+MinhaLista.mostrausuario()
