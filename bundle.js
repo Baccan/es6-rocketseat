17a529e22765d5f35835e0994213e3e028e2dbc4
Gustavo Baccan Gomes <gustavobaccang@gmail.com>
2019-06-20 16:25:42 -0300

    Template Literals

--- bundle.js
+++ bundle.js
@@ -1,77 +1,12 @@
 "use strict";
 
-function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; var ownKeys = Object.keys(source); if (typeof Object.getOwnPropertySymbols === 'function') { ownKeys = ownKeys.concat(Object.getOwnPropertySymbols(source).filter(function (sym) { return Object.getOwnPropertyDescriptor(source, sym).enumerable; })); } ownKeys.forEach(function (key) { _defineProperty(target, key, source[key]); }); } return target; }
-
-function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
-
-function _objectWithoutProperties(source, excluded) { if (source == null) return {}; var target = _objectWithoutPropertiesLoose(source, excluded); var key, i; if (Object.getOwnPropertySymbols) { var sourceSymbolKeys = Object.getOwnPropertySymbols(source); for (i = 0; i < sourceSymbolKeys.length; i++) { key = sourceSymbolKeys[i]; if (excluded.indexOf(key) >= 0) continue; if (!Object.prototype.propertyIsEnumerable.call(source, key)) continue; target[key] = source[key]; } } return target; }
-
-function _objectWithoutPropertiesLoose(source, excluded) { if (source == null) return {}; var target = {}; var sourceKeys = Object.keys(source); var key, i; for (i = 0; i < sourceKeys.length; i++) { key = sourceKeys[i]; if (excluded.indexOf(key) >= 0) continue; target[key] = source[key]; } return target; }
-
-// Operadores rest/spread
-// deve-se utilizar o plugin do babel: "@babel/plugin-proposal-object-rest-spread"
-// REST - utilizado para pegar o "resto" das propriedades
-var usuario = {
-  nome: "Baccan",
-  idade: 21,
-  empresa: "Casa"
-};
-
-var nome = usuario.nome,
-    resto = _objectWithoutProperties(usuario, ["nome"]);
-
-console.log(nome);
-console.log(resto); // pode-se aplicar em vetores
-
-var arr = [1, 2, 3, 4];
-var a = arr[0],
-    b = arr[1],
-    c = arr.slice(2);
-console.log(a);
-console.log(b);
-console.log(c); // para parâmetros de funções:
-// ao invés de:
+// Template Literals
+var nome = "Baccan";
+var idade = 21; // Não faça isso...
 
 /*
-    function soma(a, b, c){
-        return a + b + c
-    }
+  console.log("Meu nome é " + nome + " e tenho " + idade + " anos agora");
 */
-// faça:
+// Faça isso:
 
-/*
-  function soma(...params) {
-    return params;
-  }
-*/
-// exemplo com reduce apra somar todos os numeros
-
-function soma() {
-  for (var _len = arguments.length, params = new Array(_len), _key = 0; _key < _len; _key++) {
-    params[_key] = arguments[_key];
-  }
-
-  return params.reduce(function (total, next) {
-    return total + next;
-  });
-}
-
-console.log(soma(1, 3, 4)); // SPREAD - responsável por propagar as informações de um objeto para outra estrutura de dados
-
-var arr1 = [1, 2, 3];
-var arr2 = [4, 5, 6]; // para juntar os arrays utilizando SPREAD
-
-var arr3 = [].concat(arr1, arr2);
-console.log(arr3); // criando um novo objeto com as propridades de outro, e trocando propriedades
-
-var usuario1 = {
-  nome: "Baccan",
-  idade: 21,
-  empresa: "Casa"
-};
-
-var usuario2 = _objectSpread({}, usuario1, {
-  nome: "Gustavo"
-});
-
-console.log(usuario2);
+console.log("Meu nome \xE9 ".concat(nome, " e tenho ").concat(idade, " anos agora"));
--- main.js
+++ main.js
@@ -1,66 +1,10 @@
-// Operadores rest/spread
-// deve-se utilizar o plugin do babel: "@babel/plugin-proposal-object-rest-spread"
+// Template Literals
+const nome = "Baccan";
+const idade = 21;
 
-// REST - utilizado para pegar o "resto" das propriedades
-const usuario = {
-  nome: "Baccan",
-  idade: 21,
-  empresa: "Casa"
-};
-
-const { nome, ...resto } = usuario;
-
-console.log(nome);
-console.log(resto);
-
-// pode-se aplicar em vetores
-const arr = [1, 2, 3, 4];
-const [a, b, ...c] = arr;
-
-console.log(a);
-console.log(b);
-console.log(c);
-
-// para parâmetros de funções:
-// ao invés de:
+// Não faça isso...
 /*
-    function soma(a, b, c){
-        return a + b + c
-    }
+  console.log("Meu nome é " + nome + " e tenho " + idade + " anos agora");
 */
-// faça:
-/*
-  function soma(...params) {
-    return params;
-  }
-*/
-// exemplo com reduce apra somar todos os numeros
-function soma(...params) {
-  return params.reduce((total, next) => total + next);
-}
-
-console.log(soma(1, 3, 4));
-
-// SPREAD - responsável por propagar as informações de um objeto para outra estrutura de dados
-
-const arr1 = [1, 2, 3];
-const arr2 = [4, 5, 6];
-
-// para juntar os arrays utilizando SPREAD
-const arr3 = [...arr1, ...arr2];
-
-console.log(arr3);
-
-// criando um novo objeto com as propridades de outro, e trocando propriedades
-const usuario1 = {
-  nome: "Baccan",
-  idade: 21,
-  empresa: "Casa"
-};
-
-const usuario2 = {
-  ...usuario1,
-  nome: "Gustavo"
-};
-
-console.log(usuario2);
+// Faça isso:
+console.log(`Meu nome é ${nome} e tenho ${idade} anos agora`);
