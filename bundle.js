fb7cd83d0096dbbaa6ef6e4436610f92fe6f349a
Gustavo Baccan Gomes <gustavobaccang@gmail.com>
2019-06-20 15:48:14 -0300

    Valores padrões

--- bundle.js
+++ bundle.js
@@ -1,32 +1,10 @@
 "use strict";
 
-var arr = [1, 3, 4, 5, 6, 8]; // function é considerada uma função anônima por não ter um nome atribuido a ela
-// const newArr = arr.map(function(item) {
-//   return item * 2;
-// });
-// logo, ela pode ser substituida por uma arrow function, como abaixo
-// const newArr = arr.map(item => {
-//   return item * 2;
-// });
-// como não possui um corpo extenso dentro da função, tendo apenas uma linha com o retorno do item * 2, podemos simplificar mais ainda
-
-var newArr = arr.map(function (item) {
-  return item * 2;
-});
-console.log(newArr); // criando funções como constantes (não recomendado)
-
-var teste = function teste() {
-  return "teste";
+var soma = function soma() {
+  var a = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 3;
+  var b = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 6;
+  return a + b;
 };
 
-console.log(teste()); // Chamando esta função pelo nome de sua variavel temos o seguinte retorno
-
-console.log(teste); // exemplo de retorno de objeto
-
-var objeto = function objeto() {
-  return {
-    array: [1, 2, 3, 4, 5]
-  };
-};
-
-console.log(objeto());
+console.log(soma(1));
+console.log(soma());
