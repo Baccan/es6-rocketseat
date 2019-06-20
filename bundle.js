29a00114c515f89b0c57ddee09daef6b1fb9a26c
Gustavo Baccan Gomes <gustavobaccang@gmail.com>
2019-06-20 15:59:24 -0300

    Desestruturação

--- bundle.js
+++ bundle.js
@@ -1,10 +1,44 @@
 "use strict";
 
-var soma = function soma() {
-  var a = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 3;
-  var b = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 6;
-  return a + b;
-};
+// Exemplo de desestruturação
+var usuario = {
+  nome: "Baccan",
+  idade: 21,
+  endereco: {
+    cidade: "São Paulo",
+    estado: "SP"
+  }
+}; // Imagine que vc precise dos seguintes valores do usuario: nome, idade, cidade
+// Você não precisa fazer algo desta forma:
 
-console.log(soma(1));
-console.log(soma());
+/*
+    console.log(usuario.nome)
+    console.log(usuario.idade)
+    console.log(usuario.endereco.cidade)
+*/
+// Utilize a desestruturação
+// No caso, estamos criando 3 constantes (nome, idade e cidade) do usuario
+
+var nome = usuario.nome,
+    idade = usuario.idade,
+    cidade = usuario.endereco.cidade;
+console.log(nome);
+console.log(idade);
+console.log(cidade); // também é possivel utilizar em funções
+// Sem desestruturação:
+
+/*
+    function mostraNome(usuario){
+        console.log(usuario.nome)
+    }
+*/
+// com desestruturação:
+
+function mostraNome(_ref) {
+  var nome = _ref.nome,
+      idade = _ref.idade,
+      cidade = _ref.endereco.cidade;
+  console.log(nome, idade, cidade);
+}
+
+mostraNome(usuario);
--- main.js
+++ main.js
@@ -1,4 +1,41 @@
-const soma = (a = 3, b = 6) => a + b;
+// Exemplo de desestruturação
+const usuario = {
+  nome: "Baccan",
+  idade: 21,
+  endereco: {
+    cidade: "São Paulo",
+    estado: "SP"
+  }
+};
 
-console.log(soma(1));
-console.log(soma());
+// Imagine que vc precise dos seguintes valores do usuario: nome, idade, cidade
+// Você não precisa fazer algo desta forma:
+/*
+    console.log(usuario.nome)
+    console.log(usuario.idade)
+    console.log(usuario.endereco.cidade)
+*/
+// Utilize a desestruturação
+// No caso, estamos criando 3 constantes (nome, idade e cidade) do usuario
+const {
+  nome,
+  idade,
+  endereco: { cidade }
+} = usuario;
+
+console.log(nome);
+console.log(idade);
+console.log(cidade);
+
+// também é possivel utilizar em funções
+// Sem desestruturação:
+/*
+    function mostraNome(usuario){
+        console.log(usuario.nome)
+    }
+*/
+// com desestruturação:
+function mostraNome({ nome, idade, endereco: { cidade } }) {
+  console.log(nome, idade, cidade);
+}
+mostraNome(usuario);
