1ad741a7f3dd10aa76c98ac05a0cf81efda0786b
Baccan <gustavobaccang@gmail.com>
2019-06-23 18:46:33 +0000

    Conceitos - Desafio 7

--- desafios/ex7.js
+++ desafios/ex7.js
@@ -1,18 +1,20 @@
-"use strict";
-
 // Para ver este desafio, altere o valor do script "desafio_conceito" no arquivo "package.json"
-
 /*
     "desafio_conceito": "babel ./desafios/ex7.js -o ./bundle.js -w"
 */
-var nome = 'Diego';
-var idade = 23;
-var usuario = {
-  nome: nome,
-  idade: idade,
-  cidade: 'Rio do Sul'
-};
-console.log(usuario);
+
+
+const nome = 'Diego';
+const idade = 23;
+
+const usuario = {
+    nome,
+    idade,
+    cidade: 'Rio do Sul'
+}
+
+console.log(usuario)
+
 /*
     const usuario = {
         nome: nome,
