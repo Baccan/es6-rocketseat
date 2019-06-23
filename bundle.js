2ca564993d920b85289feff9326b3874be5b7c09
Baccan <gustavobaccang@gmail.com>
2019-06-23 18:43:35 +0000

    Conceitos - Desafio 6

--- bundle.js
+++ bundle.js
@@ -1,58 +1,11 @@
 "use strict";
 
-function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; var ownKeys = Object.keys(source); if (typeof Object.getOwnPropertySymbols === 'function') { ownKeys = ownKeys.concat(Object.getOwnPropertySymbols(source).filter(function (sym) { return Object.getOwnPropertyDescriptor(source, sym).enumerable; })); } ownKeys.forEach(function (key) { _defineProperty(target, key, source[key]); }); } return target; }
-
-function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
-
 // Para ver este desafio, altere o valor do script "desafio_conceito" no arquivo "package.json"
 
 /*
-    "desafio_conceito": "babel ./desafios/ex5.js -o ./bundle.js -w"
+    "desafio_conceito": "babel ./desafios/ex6.js -o ./bundle.js -w"
 */
-// 5.1 Rest
-var array = [1, 2, 3, 4, 5, 6];
-var x = array[0],
-    y = array.slice(1);
-console.log(x); // 1
+var usuario = 'Diego';
+var idade = 23; // console.log('O usuário ' + usuario + ' possui ' + idade + ' anos');
 
-console.log(y); // [2, 3, 4, 5, 6]
-// function soma...
-
-var soma = function soma() {
-  for (var _len = arguments.length, params = new Array(_len), _key = 0; _key < _len; _key++) {
-    params[_key] = arguments[_key];
-  }
-
-  return params.reduce(function (total, next) {
-    return total + next;
-  });
-};
-
-console.log(soma(1, 2, 3, 4, 5, 6)); // 21
-
-console.log(soma(1, 2)); // 3
-// 5.2 Spread
-
-var usuario = {
-  nome: 'Diego',
-  idade: 23,
-  endereco: {
-    cidade: 'Rio do Sul',
-    uf: 'SC',
-    pais: 'Brasil'
-  }
-};
-
-var usuario2 = _objectSpread({}, usuario, {
-  nome: "Gabriel"
-});
-
-var usuario3 = _objectSpread({}, usuario, {
-  endereco: _objectSpread({}, usuario.endereco, {
-    cidade: 'Lontras'
-  })
-});
-
-console.log(usuario);
-console.log(usuario2);
-console.log(usuario3);
+console.log("O usu\xE1rio ".concat(usuario, " possui ").concat(idade, " anos"));
---
+++ desafios/ex6.js
@@ -0,0 +1,9 @@
+// Para ver este desafio, altere o valor do script "desafio_conceito" no arquivo "package.json"
+/*
+    "desafio_conceito": "babel ./desafios/ex6.js -o ./bundle.js -w"
+*/
+
+const usuario = 'Diego';
+const idade = 23;
+// console.log('O usuário ' + usuario + ' possui ' + idade + ' anos');
+console.log(`O usuário ${usuario} possui ${idade} anos`);
